c616c8c288aee180a948e58b701f06485b9450ea
BOBsoft75 <bobsoft_spb@mail.ru>
2022-09-19 11:42:06 +0300

    DZ9

---
+++ DZ9_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+Console.WriteLine("Введите число M: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+string Numbers(int a, int b)
+{
+    if (a == b) return Convert.ToString(a);
+    if (a > b) return a + " " + Numbers(a - 1, b);
+    else return a + " " + Numbers(a + 1, b);
+}
+Console.Write(Numbers(num1,num2));
---
+++ DZ9_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M:");
+int numberM = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число M:");
+int numberN = int.Parse(Console.ReadLine());
+
+void NumberSum (int numberM, int numberN, int summa)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {summa}"); 
+        return;
+    }
+    summa = summa + (numberM++);
+    NumberSum(numberM, numberN, summa);
+}
+NumberSum(numberM, numberN, 0);
